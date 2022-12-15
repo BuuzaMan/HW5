443d888d597952cb4382f8cdbe488daefb1da102
BuuzaMan <batueveuror@gmail.com>
2022-12-15 22:45:46 +0800

    fix

---
+++ Homework10/script.js
@@ -0,0 +1,45 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+
+let i = 0;
+for (; i < 5; i++){
+  if (i==5) break;
+  console.log(i);
+}
+
+
+ let ingredients = {
+  mutton: 1020,
+  horsemeat:2250,
+  beef:400,
+  onion:20,
+  salt:10,
+  pepper:5,
+  salmon:225,
+  cottageCheese:150,
+  cucumber:50,
+  rice:25,
+  sourCream:100,
+  flour:25,
+}
+ let buuza = {
+   name: 'buuza',
+   ingredients: ['mutton', 'horsemeat', 'beef', 'onion', 'salt', 'pepper' ],
+   price: 1000,
+ };
+ let sushi = {
+  name: 'sushi',
+  ingredients: ['salmon', 'cottageCheese', 'cucumber', 'rice'],
+  price: 600,
+ }
+ let salamat = {
+  name: 'salamat',
+  ingredients: ['flour', 'sourCream', 'salt'],
+  price: 250,
+
+ }
+ let menu=['buuza','sushi','salamat'];
+ 
