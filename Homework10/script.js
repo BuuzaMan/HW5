679ea4253f918ddf93f4eea6dfe431fc9f470654
BuuzaMan <batueveuror@gmail.com>
2022-12-15 22:15:49 +0800

    upd

---
+++ Homework10/script.js
@@ -0,0 +1,45 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 == 0) {
+      console.log(i);
+    }
+  }
+
+let i = 0;
+for (; i < 5; i++){
+  if (i==5) break;
+  console.log(i);
+}
+
+
+ let ingredients = ['mutton', 'horsemeat', 'beef', 'onion', 'salt', 'pepper', 'salmon', 'cottaheCheese', 'cucumber', 'rice', 'sourCream', 'flour'];
+ ingredients[0] = 1020;
+ ingredients[1] = 2250;
+ ingredients[2] = 400;
+ ingredients[3] = 20;
+ ingredients[4] = 10;
+ ingredients[5] = 5;
+ ingredients[6] = 225;
+ ingredients[7] = 150;
+ ingredients[8] = 50;
+ ingredients[9] = 25;
+ ingredients[10] = 100;
+ ingredients[11] = 25;
+
+ let buuza = {
+   name: 'buuza',
+   ingredients: ['mutton', 'horsemeat', 'beef', 'onion', 'salt', 'pepper' ],
+   price: 1000,
+ };
+ let sushi = {
+  name: 'sushi',
+  ingredients: ['salmon', 'cottageCheese', 'cucumber', 'rice'],
+  price: 600,
+ }
+ let salamat = {
+  name: 'salamat',
+  ingredients: ['flour', 'sourCream', 'salt'],
+  price: 250,
+
+ }
+ let menu=['buuza','sushi','salamat'];
+ 
--- Homework8/script.js
+++ Homework8/script.js
@@ -1,74 +1,92 @@
 let mutton={
- weight: 1500,
- price: 1020,
+ name: 'mutton',
+ price: 1020
 }
 let horsemeat={
- weight: 1500,
- price: 2250,   
+ name: 'horsemeat',
+ price: 2250  
 }
-let pork={
- weight: 750,
- price: 400,   
+let beef={
+ name: 'beef',
+ price: 400   
 }
 let onion={
- weight: 200,
- price: 20,   
+ name: 'onion',   
+ price: 20 
 }
 let salt={
- weight: 15,
- price: 10,   
+ name: 'salt',   
+ price: 10   
 }
 let pepper={
- weight: 10,
- price: 5,   
+ name: 'pepper',   
+ price: 5   
 }
 let salmon={
- weight: 150,
- price: 225,   
+ name: 'salmon',
+ price: 225   
 }
 let cottageCheese={
- weight: 50,
- price:  150,
+ name: 'cottageCheese',   
+ price:  150
 }
 let cucumber={
- weight: 100,
- price: 50,   
+ name: 'cucumber',
+ price: 50   
 }
 let rice={
- weight: 300,
- price: 25,   
+ name: 'rice',
+ price: 25   
 }
 let sourCream={
- weight: 80,
- price: 100,   
+ name: 'sourCream',   
+ price: 100   
 }
 let flour={
- weight: 80,
- price: 25,   
+ name: 'flour',
+ price: 25   
 }
 
-let buuza=['mutton', 'horsemeat', 'pork', 'onion', 'salt', 'pepper'];
-let sushi=['salmon', 'cottageCheese', 'cucumber', 'rice'];
-let salamat=['sourCream', 'flour', 'salt'];
+let buuza = {
+ ingredients: [mutton.name, horsemeat.name, beef.name, onion.name, salt.name, pepper.name],
+ costPrice: mutton.price+horsemeat.price+beef.price+onion.price+salt.price+pepper.price,
+ margin: 1.5 
+};
+JSON.stringify(buuza.ingredients)
+JSON.stringify(buuza.costPrice)
+let sushi = {
+ ingredients: [salmon.name, cottageCheese.name, cucumber.name, rice.name],
+ costPrice: salmon.price+cottageCheese.price+cucumber.price+rice.price,
+ margin: 1.5
+};
+JSON.stringify(sushi.ingredients)
+JSON.stringify(sushi.costPrice)
+let salamat={
+ ingredients: [flour.name, sourCream.name, salt.price],
+ costPrice: flour.price+sourCream.price+salt.price,
+ margin: 1.5
+};
+JSON.stringify(salamat.price)
+JSON.stringify(salamat.costPrice)
+
 let menu=['buuza','sushi','salamat'];
-buuza.splice (0,1);
-menu.splice (0,1);
+menu.shift (0);
 menu.unshift ('buuza');
+buuza.ingredients.splice(0,1);
 menu.splice (1,2);
 alert (menu);
+alert (buuza.ingredients);
 
-let costPriceBuuza=(mutton.price+horsemeat.price+pork.price+onion.price+salt.price+pepper.price);
-let costPriceSalamat=(sourCream.price+flour.price+salt.price);
-let costPriceSushi=(salmon.price+cottageCheese.price+cucumber.price+rice.price);
-let CostPrice=(costPriceBuuza+costPriceSalamat+costPriceSushi);
-
-let newPriceBuuza=(costPriceBuuza * 1.5);
-let newPriceSalamat=(costPriceSalamat * 1.5);
-let newPriceSushi=(costPriceSushi * 1.5);
-
-let earningsBuuza=(newPriceBuuza-costPriceBuuza);
-let earningsSalamat=(newPriceSalamat-costPriceSalamat);
-let earningsSushi=(newPriceSushi-costPriceSushi);
+// Общая себестоимость
+let CostPrice=buuza.costPrice+salamat.costPrice+sushi.costPrice;
+// Стоимость блюда в меню
+let newPriceBuuza = buuza.costPrice * buuza.margin;
+let newPriceSalamat = salamat.costPrice * salamat.margin;
+let newPriceSushi = sushi.costPrice * sushi.margin;
+// Расчет выгоды
+let earningsBuuza=newPriceBuuza-buuza.costPrice;
+let earningsSalamat=newPriceSalamat-salamat.costPrice
+let earningsSushi=newPriceSushi-sushi.costPrice;
 
 alert (`В итоге мы имеем выгоду с продажи бууз ${earningsBuuza}, с продажи Саламата ${earningsSalamat}, с продажи суши ${earningsSushi} `);
 
